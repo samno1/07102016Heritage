d354972ff07d4ac33a9dac07bd4327409a0ad21f
samno1 <samno1@yandex.ru>
2016-10-08 04:30:36 +0300

    Initial commit

---
+++ src/CircleAndCylinder/Circle.java
@@ -0,0 +1,38 @@
+package CircleAndCylinder;
+
+/**
+ * Created by sam1 on 08.10.16.
+ */
+public class Circle {
+    private double radius;
+    private String color;
+    public Circle(){
+        radius=1.0;
+        color="red";
+    }
+    public Circle(double radius){
+        this.radius=radius;
+    }
+    public Circle(double radius,String color){
+        this.radius=radius;
+        this.color=color;
+    }
+    public void setRadius(double radius){
+        this.radius=radius;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public double getRadius(){
+        return radius;
+    }
+    public String getColor(){
+        return color;
+    }
+    public double getArea(){
+        return radius*radius*Math.PI;
+    }
+    public String toString(){
+        return " Circle[radius= "+radius+" color= "+color+"]";
+    }
+}
---
+++ src/CircleAndCylinder/Cylinder.java
@@ -0,0 +1,35 @@
+package CircleAndCylinder;
+
+/**
+ * Created by sam1 on 08.10.16.
+ */
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder(){
+        super();
+        height=1.0;
+    }
+    public Cylinder(double height){
+        super();
+        this.height=height;
+    }
+    public Cylinder(double radius, double height){
+        super(radius,"");
+        this.height=height;
+    }
+    public Cylinder(double radius, double height,String color){
+        super(radius,color);
+        this.height=height;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public  void  setHeight(double height){
+        this.height=height;
+    }
+    public double getVolume(){
+        return getArea()*height;
+    }
+
+}
