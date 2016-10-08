ef3a72a8b2ce5ac5888bb9343e1be3bb08afffdd
samno1 <samno1@yandex.ru>
2016-10-08 04:33:38 +0300

    OOP

--- src/CircleAndCylinder/CylinderTest.java
+++ src/CircleAndCylinder/CylinderTest.java
@@ -14,15 +14,12 @@ public class CylinderTest {
                 + " base area=" + c1.getArea()
                 + " volume=" + c1.getVolume()+" color= "+c1.getColor());
 
-        // Declare and allocate a new instance of cylinder
-        //   specifying height, with default color and radius
+
         Cylinder c2 = new Cylinder(10.0);
         System.out.println("Cylinder:radius=" + c2.getRadius()+ " height=" + c2.getHeight()
                 + " base area=" + c2.getArea()
                 + " volume=" + c2.getVolume()+" color= "+c2.getColor());
 
-        // Declare and allocate a new instance of cylinder
-        //   specifying radius and height, with default color
         Cylinder c3 = new Cylinder(3.0, 10.0,"black");
         System.out.println("Cylinder:radius=" + c3.getRadius()
                 + " height=" + c3.getHeight()
