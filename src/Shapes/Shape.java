4cc9c9779c2b0677fa0c28c56d11ae0a106f762b
samno1 <samno1@yandex.ru>
2016-10-12 01:35:27 +0300

    OOP

--- src/Shapes/Shape.java
+++ src/Shapes/Shape.java
@@ -4,5 +4,35 @@ package Shapes;
  * Created by sam1 on 12.10.16.
  */
 public class Shape {
+    private String color="red";
+    private  boolean filled=true;
+
+    public Shape(){
+        this.color=color;
+        this.filled=false;
+    }
+
+    public Shape(String color,boolean filled){
+        this.color=color;
+        this.filled=filled;
+    }
+    public String getColor(){
+        return color;
+    }
+    public boolean isFilled(){
+        return filled;
+    }
+    public void setColor(String color){
+        this.color=color;
+    }
+    public void setFilled(boolean filled){
+        this.filled=filled;
+    }
+    public  String toString(){
+        String a;
+        if (filled) System.out.println("filled");
+        else System.out.println("not filled");
+        return "Shape with "+color+"color and"+filled;
+    }
 
 }
---
+++ src/Shapes/TestDridve.java
@@ -0,0 +1,7 @@
+package Shapes;
+
+/**
+ * Created by sam1 on 12.10.16.
+ */
+public class TestDridve {
+}
